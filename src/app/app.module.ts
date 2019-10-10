52ea942f3a59e2a5590a9a1ee2cb464dab4b8fd5
Fabrizio De Cicco <fabrizio.decicco@deltatre.com>
2019-10-10 15:11:29 +0200

    Introduced forms basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { UserSettingFormComponent } from './user-setting-form/user-setting-form.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, UserSettingFormComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
