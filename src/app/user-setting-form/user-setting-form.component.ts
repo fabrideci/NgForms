111e97d34ee8a4e2fe8fb0a1166fd644ee6a4e77
Fabrizio De Cicco <fabrizio.decicco@deltatre.com>
2019-10-11 15:27:19 +0200

    Introduced data binding in ng forms

--- src/app/user-setting-form/user-setting-form.component.ts
+++ src/app/user-setting-form/user-setting-form.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { UserSettings } from "../data/user-settings";
 
 @Component({
-  selector: 'app-user-setting-form',
-  templateUrl: './user-setting-form.component.html',
-  styleUrls: ['./user-setting-form.component.css']
+  selector: "app-user-setting-form",
+  templateUrl: "./user-setting-form.component.html",
+  styleUrls: ["./user-setting-form.component.css"]
 })
 export class UserSettingFormComponent implements OnInit {
+  originalUserSettings: UserSettings = {
+    name: "Milton",
+    emailOffers: true,
+    interfaceStyle: "dark",
+    subscriptionType: "Annual",
+    notes: "here are some notes..."
+  };
 
-  constructor() { }
+  userSettings: UserSettings = { ...this.originalUserSettings };
 
-  ngOnInit() {
-  }
+  constructor() {}
 
+  ngOnInit() {}
 }
